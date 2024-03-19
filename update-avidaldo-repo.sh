c1543a7c4d19de8d659e84c8220dcbd87f396963
bieito.studies.ubuntu.laptop <bieito.studies.ubuntu.laptop@gmail.com>
2024-03-19 20:26:44 +0100

    add script to update avidaldo repo

---
+++ update-avidaldo-repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Cambia al directorio de tu repositorio de Git
+cd machine-learning-python/
+
+# Realiza un fetch para obtener las actualizaciones de las ramas remotas
+echo "Realizando git fetch..."
+git fetch
+
+# Realiza un pull para fusionar las actualizaciones en la rama actual
+echo "Realizando git pull..."
+git pull
+
+echo "Fetch y pull completados."
+cd ../
